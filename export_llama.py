8cff8a01de37171b754a2357726bdab69a3d357d
Luchang Li <34341633+luchangli03@users.noreply.github.com>
2023-08-05 20:47:50 +0800

    support Qwen

--- export_llama.py
+++ export_llama.py
@@ -80,7 +80,7 @@ def export_embeding(embed_model, config, args, model_name):
     )
 
 
-class DecoderLayersWrapper(nn.Module):
+class DecoderLayersWrapperLlama(nn.Module):
     def __init__(self, layers, config):
         super().__init__()
         self.layers = layers
@@ -113,6 +113,37 @@ class DecoderLayersWrapper(nn.Module):
         return hidden_states, *kv_caches_out
 
 
+class DecoderLayersWrapperQwen(nn.Module):
+    def __init__(self, layers, config):
+        super().__init__()
+        self.layers = layers
+        self.config = config
+
+    def forward(
+        self,
+        hidden_states,
+        attention_mask,
+        # past_key_values_in format is [key0, value0, key1, value1, etc.]
+        past_key_values_in,
+        output_attentions=False,
+        use_cache=True,
+    ):
+        kv_caches_out = []
+        layer_num = len(self.layers)
+        for i in range(layer_num):
+            layer = self.layers[i]
+            hidden_states, kv_cache = layer(
+                hidden_states=hidden_states,
+                attention_mask=attention_mask,
+                layer_past=[past_key_values_in[i * 2], past_key_values_in[i * 2 + 1]],
+                output_attentions=output_attentions,
+                use_cache=use_cache)
+
+            past_key, past_value = kv_cache
+            kv_caches_out.extend([past_key, past_value])
+        return hidden_states, *kv_caches_out
+
+
 def export_decoders(decoder_layers, config, dtype, args, model_name):
     """
     Note
@@ -123,34 +154,46 @@ def export_decoders(decoder_layers, config, dtype, args, model_name):
     onnx_file_name = os.path.join(args.out_dir, f"{model_name}.onnx")
 
     hidden_size = config.hidden_size
+    layer_num = len(decoder_layers)
+    head_num = config.num_attention_heads
+    hidden_size1 = hidden_size // head_num
+    print("layer_num:", layer_num, hidden_size1)
 
     batch = 1
     N = 1
     sumN = 32
     lastN = sumN - N
 
-    layer_num = len(decoder_layers)
+    if not args.model_type:
+        decoder_layers_wrapper = DecoderLayersWrapperLlama(decoder_layers, config)
 
-    head_num = config.num_attention_heads
-    hidden_size1 = hidden_size // head_num
+        hidden_in = torch.randn([batch, N, hidden_size], dtype=dtype).to(args.device)
+        attention_mask = torch.randn([batch, 1, N, sumN], dtype=dtype).to(args.device)
+        position_ids = torch.ones([batch, N], dtype=torch.int64).to(args.device)
 
-    print("layer_num:", layer_num, hidden_size1)
+        in_names = ["hidden_in", "attention_mask", "position_ids"]
 
-    decoder_layers_wrapper = DecoderLayersWrapper(decoder_layers, config)
+        dynamic_axes = {
+            'hidden_in': {1: 'N', },
+            'attention_mask': {1: 'N', 2: "sumN"},
+            "position_ids": {1: 'N', },
+        }
 
-    hidden_in = torch.randn([batch, N, hidden_size], dtype=dtype).to(args.device)
-    attention_mask = torch.randn([batch, 1, N, sumN], dtype=dtype).to(args.device)
-    position_ids = torch.ones([batch, N], dtype=torch.int64).to(args.device)
+    elif args.model_type == "Qwen":
+        decoder_layers_wrapper = DecoderLayersWrapperQwen(decoder_layers, config)
+
+        hidden_in = torch.randn([batch, N, hidden_size], dtype=dtype).to(args.device)
+        attention_mask = torch.randn([batch, 1, 1, sumN], dtype=dtype).to(args.device)
+
+        in_names = ["hidden_in", "attention_mask"]
+
+        dynamic_axes = {
+            'hidden_in': {1: 'N', },
+            'attention_mask': {1: 'N', 2: "sumN"},
+        }
 
     kv_caches_in = []
-    in_names = ["hidden_in", "attention_mask", "position_ids"]
     out_names = ["hidden_out"]
-
-    dynamic_axes = {
-        'hidden_in': {1: 'N', },
-        'attention_mask': {1: 'N', 2: "sumN"},
-        "position_ids": {1: 'N', },
-    }
 
     kv_cache_in_shape = [batch, head_num, lastN, hidden_size1]
     kv_cache_dyn_axes = {2: "lastSum"}
@@ -169,9 +212,14 @@ def export_decoders(decoder_layers, config, dtype, args, model_name):
         dynamic_axes[f"past_key_in{i}"] = kv_cache_dyn_axes
         dynamic_axes[f"past_value_in{i}"] = kv_cache_dyn_axes
 
+    if not args.model_type:
+        input_datas = (hidden_in, attention_mask, position_ids, kv_caches_in)
+    elif args.model_type == "Qwen":
+        input_datas = (hidden_in, attention_mask, kv_caches_in)
+
     torch.onnx.export(
         decoder_layers_wrapper,
-        (hidden_in, attention_mask, position_ids, kv_caches_in),
+        input_datas,
         onnx_file_name,
         opset_version=args.opset,
         do_constant_folding=True,
@@ -196,11 +244,21 @@ def export_llama(args):
     print(f"finish load model from {args.model_path}")
     config = model.config
 
-    # default configure for llama like models
-    lm_head_model = model.lm_head
-    embeding_model = model.model.embed_tokens
-    norm_model = model.model.norm
-    decoder_layers = model.model.layers
+    if not args.model_type:
+        # default configure for llama like models
+        lm_head_model = model.lm_head
+        embeding_model = model.model.embed_tokens
+        norm_model = model.model.norm
+        decoder_layers = model.model.layers
+    elif args.model_type == "Qwen":
+        # support alibaba Qwen
+        lm_head_model = model.lm_head
+        embeding_model = model.transformer.wte
+        norm_model = model.transformer.ln_f
+        decoder_layers = model.transformer.h
+        args.kv_cache_format = 1
+    else:
+        raise ValueError("invalid model_type")
 
     print(f"begin export_lm_head")
     export_lm_head(lm_head_model, config, dtype, args, "lm_head")
@@ -236,9 +294,12 @@ if __name__ == "__main__":
     parser.add_argument('-d', '--device', required=False, type=str, default="cuda")
     # supported dtype: ["float32", "float16", "bfloat16"]
     parser.add_argument('-p', '--dtype', required=False, type=str, default="float16")
+    # 0: export all decoders into one onnx. >0: export multiple onnx files, and each onnx has decoder_pack_size layers
     parser.add_argument('--decoder_pack_size', required=False, type=int, default=0)
     # 0 means [batch, head, seq, hidden], 1 means [batch, seq, head, hidden]
     parser.add_argument('--kv_cache_format', required=False, type=int, default=0)
+    # default model_type is llama_hf, other model_type such as Qwen, Baichuan ban be supported
+    parser.add_argument('--model_type', required=False, type=str, default="")
 
     args = parser.parse_args()
 
